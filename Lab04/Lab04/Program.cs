b31652880ce499262fe776475d494964c5994eab
chatwarin <chatwarin97@gmail.com>
2017-10-30 21:16:13 +0700

    if 1

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.IO;
-public class ExceptionLearning
+public class IfLearning
 {
     public static void Main()
     {
-        int a = 10;
-        int b = 20;
-        int c = add(a, b);
-    }
-    private static int add(int a, int b)
-    {
-        throw new NotImplementedException();
+        int a = 2;
+        if (a == 2)
+        {
+            Console.WriteLine("execute this line");
+        }
+        if (a < 2)
+        {
+            Console.WriteLine("execute this line too");
+        }
+        Console.WriteLine("execute next line");
     }
 }
+
+
+
 
 
 
