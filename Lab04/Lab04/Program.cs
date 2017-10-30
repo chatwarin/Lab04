a8fe2bc515f27a6bf80ea9df1eca2769373e1637
chatwarin <chatwarin97@gmail.com>
2017-10-30 21:07:39 +0700

    try catch 5

--- Lab04/Lab04/Program.cs
+++ Lab04/Lab04/Program.cs
@@ -3,11 +3,13 @@ public class TryCatch
 {
     static void Main(string[] args)
     {
-        int a = int.MaxValue;
-        a *= 2;
+        int a = 0;
+        int b = 10;
+        b /= a;
         Console.WriteLine(a);
     }
 }
+
 
 
 
